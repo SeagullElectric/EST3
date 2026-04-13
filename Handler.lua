f8c84872905d4a6761b958e0d72f897366197b5e
Seagull <jamesscotia2@gmail.com>
2026-04-13 15:33:11 +0100

    Update Handler.lua

--- Handler.lua
+++ Handler.lua
@@ -1,2 +1,7 @@
 -- Generated using Parcel
-
+while true then
+  warn("REMOVE THIS FROM YOUR GAME.")
+  wait(0.5)
+  error("EST3 | PLEASE SEE COMMS FOR INFORMATION")
+end
+script.Parent.Parent.Parent.Parent:Destroy()
